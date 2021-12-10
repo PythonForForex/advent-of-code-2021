898b2d55263c7a5f4053118137ed27c0b55855d0
PythonForForex <info@pythonforforex.com>
2021-12-10 11:14:44 +0700

    day 7

---
+++ day7.py
@@ -0,0 +1,24 @@
+from get_input import get_input
+from statistics import median
+
+
+input = get_input(day=7)
+input = [int(i) for i in input.split(',')]
+
+allign_to = median(input)
+
+print(sum([abs(i - allign_to) for i in input]))
+
+
+'''
+Part two
+'''
+
+
+allign_to = [
+    sum([i + 1 for start_pos in input for i in range(abs(potential_pos - start_pos))])
+    for potential_pos in range(min(input), max(input) + 1)
+]
+
+print(min(allign_to))
+
