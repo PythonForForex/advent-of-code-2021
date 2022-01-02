2132fd92819cb139d868c5cbd6b6458985578c7e
PythonForForex <info@pythonforforex.com>
2022-01-03 01:34:42 +0700

    refactored

--- day12.py
+++ day12.py
@@ -7,50 +7,41 @@ raw_input = raw_input.splitlines()
 paths_map = {}
 
 for line in raw_input:
-	a, b = line.split('-')
-
-	if b != 'start':
-		paths_map.setdefault(a, []).append(b)
-	if b != 'end' and a != 'start':
-		paths_map.setdefault(b, []).append(a)
+    a, b = line.split('-')
+    paths_map.setdefault(a, []).append(b)
+    paths_map.setdefault(b, []).append(a)
 
 
 def main(custom_func):
-	paths = [['start']]
-	completed = []
+    paths = [['start']]
+    completed = []
 
-	while paths:
-		path = paths.pop()
-		current_pos = path[-1]
-		path_from_here = paths_map.get(current_pos)
+    while paths:
+        path = paths.pop()
+        current_pos = path[-1]
+        path_from_here = paths_map.get(current_pos)
 
-		if not path_from_here:
-			continue
+        for potential in path_from_here:
+            if potential == 'end' and path not in completed:
+                completed.append(path)
+                continue
 
-		for potential in path_from_here:
-			if potential == 'end' and path not in completed:
-				completed.append(path)
-				continue
+            if potential.islower() and custom_func(potential, path):
+                continue
 
-			if potential.islower() and custom_func(potential, path):
-				continue
+            paths.append(path + [potential])
 
-			paths.append(path + [potential])
-
-	return len(completed)
+    return len(completed)
 
 
 def part_one(potential, path):
-	if potential in path:
-		return True
+    return potential in path
 
 
 def part_two(potential, path):
-	all_lower = [i for i in path if i.islower()]
-	restriced = [i for i in set(all_lower) if all_lower.count(i) == 2]
-
-	if restriced and potential in all_lower:
-		return True
+    all_lower = [i for i in path if i.islower()]
+    restriced = [i for i in set(all_lower) if all_lower.count(i) == 2]
+    return restriced and potential in all_lower or potential == 'start'
 
 
 print(main(part_one))
